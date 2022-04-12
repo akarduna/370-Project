7dd919b8ff61a5102d52fa85bb24926d864a5ef3
Garrett Smetana <garrettsmet@yahoo.com>
2022-04-11 18:26:33 -0600

    added extremely basic home page

--- frontend/src/components/mainPage.jsx
+++ frontend/src/components/mainPage.jsx
@@ -1,3 +1,26 @@
+import { Fragment, useState } from "react";
+import { Button, TextField, Typography } from "@mui/material";
+
 export default function MainPage() {
-	return <h1> Home Page </h1>;
+	const [val, setVal] = useState("Default");
+
+	const handleTypeChange = (event) => {
+		setVal(event.target.value);
+	};
+
+	const handleSubmit = () => {
+		console.log("Submitted: " + val);
+	};
+
+	return (
+		<Fragment>
+			<Typography variant="h3">League of Legends Heartbeat Data</Typography>
+			<TextField
+				value={val}
+				onChange={handleTypeChange}
+				placeholder="Enter match ID"
+			/>
+			<Button onClick={handleSubmit}>Submit</Button>
+		</Fragment>
+	);
 }
