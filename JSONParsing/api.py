65b44be62f8d13761fc4414a0634bbde97b5056d
akarduna <alexkarduna@gmail.com>
2022-04-24 15:33:00 -0600

    api working for game data

--- JSONParsing/api.py
+++ JSONParsing/api.py
@@ -1,27 +1,16 @@
+import json
 from flask import Flask, jsonify, request
 import GameDataPuller
+import converter
 
 app = Flask(__name__)
 
-incomes = [
-  { 'description': 'salary', 'amount': 5000 }
-]
-
-
-@app.route('/incomes')
-def get_incomes():
-    return jsonify(incomes)
-
-
-@app.route('/incomes', methods=['POST'])
-def add_income():
-  incomes.append(request.get_json())
-  return '', 204
-
 @app.route('/RIOT_DATA', methods=['POST'])
 def query_riot_api():
-    json = request.get_json()
-    print(json["summoner_name"])
-    print(json["game_number"])
-    GameDataPuller.run(int(json["game_number"]), json["summoner_name"])
-    return " ", 200
+    data = request.get_json()
+    print(data["summoner_name"])
+    print(data["game_number"])
+    GameDataPuller.run(int(data["game_number"]), data["summoner_name"])
+    converter.run()
+    f = open("gamedata.json")
+    return json.load(f), 200
