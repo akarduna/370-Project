1f04a577b5d6ee59b49f65ac114909a8760fc529
akarduna <alexkarduna@gmail.com>
2022-04-24 15:24:50 -0600

    runs GameDataPuller from api

--- JSONParsing/api.py
+++ JSONParsing/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import GameDataPuller
 
 app = Flask(__name__)
 
@@ -9,10 +10,18 @@ incomes = [
 
 @app.route('/incomes')
 def get_incomes():
-  return jsonify(incomes)
+    return jsonify(incomes)
 
 
 @app.route('/incomes', methods=['POST'])
 def add_income():
   incomes.append(request.get_json())
   return '', 204
+
+@app.route('/RIOT_DATA', methods=['POST'])
+def query_riot_api():
+    json = request.get_json()
+    print(json["summoner_name"])
+    print(json["game_number"])
+    GameDataPuller.run(int(json["game_number"]), json["summoner_name"])
+    return " ", 200
