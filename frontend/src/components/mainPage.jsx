728bb0eedf719b8c8170da911e155b62c6b57de6
Garrett Smetana <garrettsmet@yahoo.com>
2022-04-25 14:31:41 -0600

    added temp timeline to test component

--- frontend/src/components/mainPage.jsx
+++ frontend/src/components/mainPage.jsx
@@ -1,8 +1,13 @@
 import { Fragment, useState } from "react";
 import { Button, TextField, Typography } from "@mui/material";
+import Timeline from "@mui/lab/Timeline";
+import TimelineDragon from "./timelineDragon";
+import TimelineTakedown from "./timelineTakedown";
+import file from "../exampledata/gamedata.csv";
+import { parseJSON } from "../functions/parseJSON";
 
 export default function MainPage() {
-	const [val, setVal] = useState("Default");
+	const [val, setVal] = useState("");
 
 	const handleTypeChange = (event) => {
 		setVal(event.target.value);
@@ -21,6 +26,11 @@ export default function MainPage() {
 				placeholder="Enter match ID"
 			/>
 			<Button onClick={handleSubmit}>Submit</Button>
+			<Timeline position="left">
+				<TimelineDragon heartrate={20} type={"Ocean"} time={"10 secs"} />
+				<TimelineTakedown heartrate={56} enemy={"Jhin"} time={"10 secs"} />
+				<TimelineDragon heartrate={90} />
+			</Timeline>
 		</Fragment>
 	);
 }
