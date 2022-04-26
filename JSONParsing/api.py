abc5b974148c2f106b3860a74f0424a0c3fd1b04
akarduna <alexkarduna@gmail.com>
2022-04-26 13:28:16 -0600

    valid call to api

--- JSONParsing/api.py
+++ JSONParsing/api.py
@@ -1,15 +1,18 @@
 import json
+from flask_cors import CORS
 from flask import Flask, jsonify, request
 import GameDataPuller
 import converter
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/RIOT_DATA', methods=['POST'])
 def query_riot_api():
+    content_type = request.headers.get('Content-Type')
+    print(content_type)
     data = request.get_json()
-    print(data["summoner_name"])
-    print(data["game_number"])
+    print(data)
     GameDataPuller.run(int(data["game_number"]), data["summoner_name"])
     converter.run()
     f = open("gamedata.json")
--- frontend/src/components/mainPage.jsx
+++ frontend/src/components/mainPage.jsx
@@ -9,6 +9,11 @@ import TimelineEnd from "./timelineEnd";
 import TimelineStart from "./timelineStart";
 import TimelineTakedown from "./timelineTakedown";
 import TimelineTakendown from "./timelineTakendown";
+import TimelineStart from "./timelineStart";
+import TimelineEnd from "./timelineEnd";
+import { Box } from "@mui/system";
+import TimelineBaron from "./timelineBaron";
+import $ from "jquery";
 
 export default function MainPage() {
 	const [val, setVal] = useState("");
@@ -27,6 +32,23 @@ export default function MainPage() {
 	};
 
 	const handleSubmit = () => {
+		const url = 'http://127.0.01:5000/RIOT_DATA';
+		$.ajax(
+			url,
+			{
+				type: `POST`,
+				contentType: "application/json",
+				data : JSON.stringify({
+					"summoner_name": "plez",
+					"game_number": 0
+				  }),
+				success: function (data, status, xhr) {
+					$('p').append('status: ' + status + ', data: ' + data);
+				},
+				error: function (jqXhr, textStatus, errorMessage) {
+						$('p').append('Error' + errorMessage);
+			}
+			});
 		if (!val) {
 			setHelpMsg(true);
 		} else {
